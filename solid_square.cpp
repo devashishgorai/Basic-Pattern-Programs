e6be203463b68092f4b93e1c3a598b6ca5ebafcc
Devashish Gorai <devashishgorai10@gmail.com>
2025-12-25 12:01:28 +0530

    Added detailed comments to solid square pattern

---
+++ solid_square.cpp
@@ -0,0 +1,22 @@
+// Pattern Name: Solid Square
+// Description : Prints a solid square star pattern
+// Author      : Devashish Gorai
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+}
