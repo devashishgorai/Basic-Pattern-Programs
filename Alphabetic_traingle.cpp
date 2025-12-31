5115adba2464b6865750df62cf897bf4fe126e1f
Devashish Gorai <devashishgorai10@gmail.com>
2025-12-31 19:29:18 +0530

    Added Alpahbetic traingle file

---
+++ Alphabetic_traingle.cpp
@@ -0,0 +1,28 @@
+// Pattern Name: Numeric right angle triangle Pattern
+// Description : Prints a right angle traingle pattern like strucutre using numbers of 1 & 0
+// Author      : Devashish Gorai
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for( int i =0; i<=n; i++){
+        for( char ch = 'A'; ch<='A'+i; ch++){
+            cout << ch <<" ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
+
+//Pattern Output;
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
